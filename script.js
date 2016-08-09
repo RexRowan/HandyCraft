e875d29b06f58e77e08d007c759e68ac8bc538ae
Tingting Zhao <RexRowan@users.noreply.github.com>
2016-08-09 10:04:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 <script>
 // Load this when the DOM is ready
 $(function(){
-  // You used .myCarousel here. 
-  // That's the class selector not the id selector,
-  // which is #myCarousel
   $('#carousel-example-generic').carousel();
 });
 </script>
